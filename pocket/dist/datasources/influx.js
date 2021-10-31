03e66d98da4583fedb4415bee727255ac973a640
Alex Firmani <jafirmani@gmail.com>
2021-10-31 08:02:29 -0700

    env

--- pocket/dist/datasources/influx.js
+++ pocket/dist/datasources/influx.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.writeAPI = exports.influxClient = void 0;
 const influxdb_client_1 = require("@influxdata/influxdb-client");
-exports.influxClient = new influxdb_client_1.InfluxDB({ url: "http://localhost:8086", token: "-QVD3pKtY_ZBdfLZYxclSA6frrzMVEaPqLlNY93dQbFSkO6RLJ-HsbA8X22xwzTaEGSlRqgaZYCBIyUwOzd-QA==" });
+exports.influxClient = new influxdb_client_1.InfluxDB({ url: "http://influx.nachonodes.com:8086", token: `${process.env.INFLUX_TOKEN}` });
 exports.writeAPI = exports.influxClient.getWriteApi("nachonodes", "nodes");
 //# sourceMappingURL=influx.js.map
--- pocket/src/datasources/influx.ts
+++ pocket/src/datasources/influx.ts
@@ -1,4 +1,4 @@
 import { InfluxDB } from '@influxdata/influxdb-client'
 
-export const influxClient = new InfluxDB({ url: "http://localhost:8086", token: "-QVD3pKtY_ZBdfLZYxclSA6frrzMVEaPqLlNY93dQbFSkO6RLJ-HsbA8X22xwzTaEGSlRqgaZYCBIyUwOzd-QA==" })
+export const influxClient = new InfluxDB({ url: "http://influx.nachonodes.com:8086", token: `${process.env.INFLUX_TOKEN}` })
 export const writeAPI = influxClient.getWriteApi("nachonodes", "nodes")
